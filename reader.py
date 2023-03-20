548ba3e3846b8d3c6491f99be629c78a6f717b19
visontail <the051425@gmail.com>
2023-03-20 11:51:37 +0100

    progress

--- reader.py
+++ reader.py
@@ -1,45 +1,64 @@
-import os
+import os, shutil
 
 
-# Folder of .XTX files goes here
-dir_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/files'
+# - newly added .XTX files path:
+dir_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/files/'
+# - already read .XTX files path:
+move_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/done/'
+
 # Allowed extesions list
 EXTENSIONS = ('.xtx', '.Xtx', '.XTx', '.XtX', '.xTx', '.xTX', '.xtX', '.XTX')
 
 # FILE GATHERING PART
 
+def getXtx(path):
+    # - scan all files in the directory
+    with os.scandir(path) as dir_entries:
+        # - use a list comprehension to filter the files by extension
+        xtx_files = [path + entry.name for entry in dir_entries if entry.name.endswith(EXTENSIONS)]
+    # - return the number and names of the XTX files
+    return len(xtx_files), xtx_files
 
-# !!!!!! todo combine the two get def into one which returns num, and names
-def getNumOfXtxFiles(path):    
-    # get a list of all files in the directory
-    files = os.listdir(path)
-    # count the number of files with the specified extension
-    num_files = sum(1 for file in files if file.endswith(EXTENSIONS))
-    return num_files
+# 'n' is the sum of xtx files & 'name' is the list of xtx files' name
+n, xtx_names = getXtx(dir_path)
+
+def readXtx(n, names):
+    # - use a list comprehension to create n empty lists
+    xtx_lists = [[] for i in range(n)]
+    # - iterate over the file names and read each file into a separate list
+    for i, name in enumerate(names):
+        with open(name, 'r') as file:
+            # - use a list comprehension to read the file into a list of lines
+            lines = [line.strip() for line in file]
+            # - add the lines to the corresponding list
+            xtx_lists[i] = lines
+    return xtx_lists
+
+print(readXtx(n,xtx_names)[0])
+print("---")
+print(readXtx(n,xtx_names)[0][1])
+print(readXtx(n,xtx_names)[0][2])
+print(readXtx(n,xtx_names)[0][3])
+print("---")
+print(readXtx(n,xtx_names)[1])
+print("----")
+print(readXtx(n,xtx_names)[2])
 
 
-def getNamesOfXtxFiles(path):
-    files = os.listdir(path)
-    # list out the names of all the files with the specified extension
-    xtx_name = [file for file in files if file.endswith(EXTENSIONS)]
-    return xtx_name
 
+"""
+MAYBE CREATE A DECORATER FOR IT
 
-# !!!!! todo for loop for file names
+move after read and post in db
 
-# ez lesz a ciklus max lépésszám
-n = getNumOfXtxFiles(dir_path)
-# ezek lesznek a nevek folyamatosan
-name = 'files/' + (getNamesOfXtxFiles(dir_path)[0])
+todo - create a def moveXtx( post to db was successfull )
 
-print(n)
-print(name)
+filename = xtx_names[1].split("/")[-1]
+old_xtx_path = dir_path + filename
+new_xtx_path = move_path + filename
+shutil.move(old_xtx_path, new_xtx_path)
 
-# READING PART
-def readFile(name):
-    with open(name, "r") as file:
-        # read the file line by line
-        data = [line.rstrip() for line in file]
-    return data
+returns a boolean done it or not 
+if not error message 
 
-print(readFile(name))
+"""
