39c7097280114d2bb30fb1a8cab248ca0d63bf41
visontail <the051425@gmail.com>
2023-03-21 16:19:33 +0100

    starting with one

--- solo.py
+++ solo.py
@@ -2,7 +2,7 @@
 # WRITE ALL CODE FOR ONE XTX FILE
 # -------------------------------
 
-import os, shutil
+import os, shutil, mysql.connector
 
 # - newly added .XTX files path:
 dir_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/files/'
@@ -12,59 +12,35 @@ move_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/done/'
 # Allowed extesions list
 EXTENSIONS = ('.xtx', '.Xtx', '.XTx', '.XtX', '.xTx', '.xTX', '.xtX', '.XTX')
 
-# FILE GATHERING PART
+# file name
+name = 'files/s113006_2022101312.xTx'
 
-def getXtx(path):
-    # - scan all files in the directory
-    with os.scandir(path) as dir_entries:
-        # - use a list comprehension to filter the files by extension
-        xtx_files = [path + entry.name for entry in dir_entries if entry.name.endswith(EXTENSIONS)]
-    # - return the number and names of the XTX files
-    return len(xtx_files), xtx_files
 
-# 'n' is the sum of xtx files & 'name' is the list of xtx files' name
-n, xtx_names = getXtx(dir_path)
+with open(name, "r") as file:
+    lines = [line.strip() for line in file]
+    data_lines = [lines[1],lines[2],lines[3],lines[-1],lines[-2]]
+    imp_info = [item.split(" ") for item in data_lines]
+    # Device information
+    dev_data = [imp_info[0][1],imp_info[1][4]]
+    # Measurement data
+    mes_data = [imp_info[-2],imp_info[-1]]
 
-def readXtx(n, names):
-    # - use a list comprehension to create n empty lists
-    xtx_lists = [[] for i in range(n)]
-    # - iterate over the file names and read each file into a separate list
-    for i, name in enumerate(names):
-        with open(name, 'r') as file:
-            # - use a list comprehension to read the file into a list of lines
-            lines = [line.strip() for line in file]
-            # - add the lines to the corresponding list
-            xtx_lists[i] = lines
-    return xtx_lists
 
-print(readXtx(n,xtx_names)[0])
-print("---")
-print(readXtx(n,xtx_names)[0][1].split(' '))
-print(readXtx(n,xtx_names)[0][2].split(' '))
-print(readXtx(n,xtx_names)[0][3].split(' '))
-print(readXtx(n,xtx_names)[0][-2].split(' '))
-print(readXtx(n,xtx_names)[0][-1].split(' '))
-print("---")
-print(readXtx(n,xtx_names)[1])
-print("----")
-print(readXtx(n,xtx_names)[2])
+print(dev_data)
+print("Mérőhely azonosító: ", dev_data[0], ", Műszer gyári szám: ", dev_data[1])
+print(mes_data)
+
+# Creates date from the given numbers
+from datetime import datetime
+date_str = mes_data[0][2] + mes_data[0][3]
+date_obj = datetime.strptime(date_str, '%y%m%d%H')
+
+print(date_obj)
 
 
 
-"""
-MAYBE CREATE A DECORATER FOR IT
-move after read and post in db
-todo - create a def moveXtx( post to db was successfull )
-returns a boolean done it or not 
-if not error message 
-
-new_xtx_path = move_path + xtx_names[1].split("/")[-1]
-shutil.move(xtx_names[1], new_xtx_path)
-"""
-
-# Importing module
-import mysql.connector
-
+# -------------------------------------------------------------------
+'''
 #Create database connection
 def conn_DB():
     # Creating connection object
@@ -76,12 +52,13 @@ def conn_DB():
     )
     # check if connection is established
     if db.is_connected():
-        print("Database connection established successfully!")
         return db
     else:
-        print("Database connection failed!")
-        return None
-
+        return print("Database connection failed!")
+'''   
+    
+    
+'''
 def create_Cursor(db):
     if db:
         cursor = db.cursor()
@@ -104,12 +81,13 @@ db.commit()
 # Print out how many was inserted
 print(cur.rowcount, "details inserted")
 
-'''
+
+
 sql = "INSERT INTO Student (Name, Roll_no) VALUES (%s, %s)"
 val = ("Ram", "85")
 cursor.execute(sql, val)
 db.commit()
-'''
+
 
 # WRITES OUT WHAT IN DB
 sql = "SELECT * FROM testFirst"
@@ -117,8 +95,10 @@ cur.execute(sql)
 myresult = cur.fetchall()
 for x in myresult:
     print (x)
-
-
 #Closing connection from server
 db.close()
 cur.close()
+
+'''
+
+
