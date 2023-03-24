84b73cd3979d4a6172887e13acc6807c7843e817
visontail <the051425@gmail.com>
2023-03-24 07:47:46 +0100

    clean up for next week

--- OOP.py
+++ OOP.py
@@ -38,6 +38,36 @@ class DB():
     pass
 
 
+# FILE GATHERING PART
+'''
+def getXtx(path):
+    # - scan all files in the directory
+    with os.scandir(path) as dir_entries:
+        # - use a list comprehension to filter the files by extension
+        xtx_files = [path + entry.name for entry in dir_entries if entry.name.endswith(EXTENSIONS)]
+    # - return the number and names of the XTX files
+    return len(xtx_files), xtx_files
+
+# 'n' is the sum of xtx files & 'name' is the list of xtx files' name
+n, xtx_names = getXtx(dir_path)
+'''
+
+
+"""
+MAYBE CREATE A DECORATER FOR IT
+
+move after read and post in db
+
+todo - create a def moveXtx( post to db was successfull )
+
+new_xtx_path = move_path + xtx_names[1].split("/")[-1]
+shutil.move(xtx_names[1], new_xtx_path)
+
+returns a boolean done it or not 
+if not error message 
+
+"""
+
 # - newly added .XTX files path:
 file_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/files/'
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, shutil
+import OOP 
 
 
 # - newly added .XTX files path:
@@ -7,32 +7,3 @@ dir_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/files/'
 move_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/done/'
 # Allowed extesions list
 EXTENSIONS = ('.xtx', '.Xtx', '.XTx', '.XtX', '.xTx', '.xTX', '.xtX', '.XTX')
-
-# FILE GATHERING PART
-
-def getXtx(path):
-    # - scan all files in the directory
-    with os.scandir(path) as dir_entries:
-        # - use a list comprehension to filter the files by extension
-        xtx_files = [path + entry.name for entry in dir_entries if entry.name.endswith(EXTENSIONS)]
-    # - return the number and names of the XTX files
-    return len(xtx_files), xtx_files
-
-# 'n' is the sum of xtx files & 'name' is the list of xtx files' name
-n, xtx_names = getXtx(dir_path)
-
-
-"""
-MAYBE CREATE A DECORATER FOR IT
-
-move after read and post in db
-
-todo - create a def moveXtx( post to db was successfull )
-
-new_xtx_path = move_path + xtx_names[1].split("/")[-1]
-shutil.move(xtx_names[1], new_xtx_path)
-
-returns a boolean done it or not 
-if not error message 
-
-"""
