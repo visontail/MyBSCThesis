5cbbee053216e0e5d99bde3394ded49a16aef841
visontail <the051425@gmail.com>
2023-04-13 14:59:50 +0200

    final

--- database.py
+++ database.py
@@ -10,6 +10,7 @@ class DataBase():
         self._database = database
         self._connection = None
         self._cursor = None
+    
     # Function to establish database connection
     def connect(self):
         try:
@@ -26,11 +27,13 @@ class DataBase():
         # - print out error if connection failed
         except mysql.connector.Error as error:
             print(f' Connection Failed! ,"{error}"')
+
     # Function for disconnecting from database
     def disconnect(self):
         if self._cursor is not None and self._connection.is_connected():
             self._cursor.close()
             self._connection.close()
+
     # Function for adding new data to database        
     def add_new_data(self, name, time, data_01, data_02):
         def execute_q(query, values):
@@ -39,12 +42,16 @@ class DataBase():
                 self._connection.commit()
             else:
                 print("Cursor not found. Check database connection!")
-        # - adding direction one measurement data
-        query = "INSERT INTO `Measurement`(`ID_station_num`, `start_time`, `end_time`, `direction`, `meas_intervall`, `traff_other`, `traff_pedestrian`, `traff_cyclist`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        values = (name, time[0], time[1], data_01[0], data_01[1], data_01[2], data_01[3], data_01[4])
-        execute_q(query, values)
-        # - adding direction two measurement data
-        query = "INSERT INTO `Measurement`(`ID_station_num`, `start_time`, `end_time`, `direction`, `meas_intervall`, `traff_other`, `traff_pedestrian`, `traff_cyclist`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        values = (name, time[0], time[1], data_02[0], data_02[1], data_02[2], data_02[3], data_02[4])
-        execute_q(query, values)
-
+        try:
+            # - adding direction one measurement data
+            query = "INSERT INTO `Measurement`(`ID_station_num`, `start_time`, `end_time`, `direction`, `meas_intervall`, `traff_other`, `traff_pedestrian`, `traff_cyclist`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            values = (name, time[0], time[1], data_01[0], data_01[1], data_01[2], data_01[3], data_01[4])
+            execute_q(query, values)
+            # - adding direction two measurement data
+            query = "INSERT INTO `Measurement`(`ID_station_num`, `start_time`, `end_time`, `direction`, `meas_intervall`, `traff_other`, `traff_pedestrian`, `traff_cyclist`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            values = (name, time[0], time[1], data_02[0], data_02[1], data_02[2], data_02[3], data_02[4])
+            execute_q(query, values)
+            return True
+        except mysql.connector.Error as mysql_error:
+            print(f"Error executing query: {mysql_error}")
+            return False
--- main.py
+++ main.py
@@ -1,85 +1,59 @@
-
+import shutil
 # importing 'file_OOP' py file as 'fc' refering to File Class
 import file_OOP as fc
 # importing 'database' py file as 'db'
 import database as db
 
 
-
 # - newly added .XTX files path:
-    # sample_path = '/users/user/desktop/fodler/'
 dir_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/files/'
 # - already read .XTX files path:
 move_path = '/Users/visontaileo/Desktop/szakdoga/PROGRAM/MyBSCThesis/done/'
 # - allowed extesions:
 EXTENSIONS = ('.xtx', '.Xtx', '.XTx', '.XtX', '.xTx', '.xTX', '.xtX', '.XTX')
 
-# - database connection parameters
-database = db.DataBase(host="localhost", username="root", password="", database="BSCthesis")
 
 # MAIN Function
 def main():
     # - read given directory and collects xtx file names
     xtx_names = fc.readDirectory(dir_path, EXTENSIONS)
-    # - connects to the database
-    database.connect()
-    i = 0
-    # - gets through every file in the directory
-    for xtx in xtx_names:
-        i +=  1
-        # - if it is the first one creates object
-        if (i == 1):
-            data = fc.File(xtx)
-        # - then just changes the file_name object
-        else:
-            data.file_name = xtx
-        # - read one file at once
-        name, time, data_01, data_02  = fc.File(xtx).read_file()
-        # - execute sql query for both direction
-        database.add_new_data(name, time, data_01, data_02)
+    # - check if there is any xtx file in directory
+    if (len(xtx_names) == 0 ):
+        print("There's no xTx file in the given directory. Please try again later!")
+    else:
+        # - create database connection object
+        database = db.DataBase(host="localhost", username="root", password="", database="BSCthesis")
+        # - connects to the database
+        database.connect()
+        i = 0
+        # - gets through every file in the directory
+        for xtx in xtx_names:
+            i +=  2
+            # - if it is the first one creates object
+            if (i == 2):
+                data = fc.File(xtx)
+            # - then just sets the file_name object
+            else:
+                data.file_name = xtx
+            # - read one file at once
+            name, time, data_01, data_02  = fc.File(xtx).read_file()
+            # - execute sql query
+            success = database.add_new_data(name, time, data_01, data_02)
+            if success:
+                # - create new path
+                new_xtx_path = move_path + xtx.split("/")[-1]
+                # - move file
+                shutil.move(xtx, new_xtx_path)
+            else:
+                print("Something went wrong. Check database!")
+                break
+        # - close down database connection
+        database.disconnect()
+        # - delete the file's name
+        del fc.File.file_name
 
-    # - finally closes down the database connection & deletes the file's name
-    database.disconnect()
-    del fc.File.file_name
 
- 
 if __name__ == "__main__":  
-    main()   
-    
-"""    
- - set first name 
-file = File(test[0])
-print(file)
-print(file.file_name)
- - add new file name
-file.file_name = test[1]
- - del file name
-del file.file_name
-
-
-# Simple check - delete later
-    def check(self):
-        print("This is all you got: ")
-        # STATION TABLE
-        self._cursor.execute("SELECT * From Station")
-        result = self._cursor.fetchall()
-        for r in result:
-            print(r)
-        print(self._cursor.rowcount, "details inserted")
-
-        
-MAYBE CREATE A DECORATER FOR IT
-
-move after read and post in db
-
-todo - create a def moveXtx( post to db was successfull )
-
-new_xtx_path = move_path + xtx_names[1].split("/")[-1]
-shutil.move(xtx_names[1], new_xtx_path)
-
-returns a boolean done it or not 
-if not error message 
-
-"""
+    main()
 
 
