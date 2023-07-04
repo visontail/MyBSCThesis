93da37e5b057686d81943931154660e41796067a
visontail <the051425@gmail.com>
2023-07-04 13:32:38 +0200

    setup 1.0

--- FRONTEND-ANGULAR/web-velo/src/app/app.module.ts
+++ FRONTEND-ANGULAR/web-velo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
