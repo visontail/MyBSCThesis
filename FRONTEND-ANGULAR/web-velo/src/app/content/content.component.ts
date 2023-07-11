cfdf529bd740e6a7b3c0dcde3e005a3218918f2b
visontail <the051425@gmail.com>
2023-07-11 10:26:41 +0200

    first test-marker implemented

--- FRONTEND-ANGULAR/web-velo/src/app/content/content.component.ts
+++ FRONTEND-ANGULAR/web-velo/src/app/content/content.component.ts
@@ -7,7 +7,7 @@ import { Loader } from '@googlemaps/js-api-loader';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  
+
   ngOnInit(): void {
     const loader = new Loader({
       apiKey: "AIzaSyCrqeOVzVOTRdPZh_VoEN1epBl04KoxJlc",
@@ -19,109 +19,117 @@ export class ContentComponent implements OnInit {
       throw new Error("The element #map wasn't found");
     }
 
-    loader.load().then(async () => {        
-      new google.maps.Map(map, {
-        center: { lat: 47, lng: 20},
-        zoom: 8,
-        styles: [
+    let position = { lat: 46.36167, lng: 17.704};
+
+    let style_sheet: [
+      {
+        "featureType": "administrative.land_parcel",
+        "stylers": [
           {
-            "featureType": "administrative.land_parcel",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "administrative.neighborhood",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "poi",
-            "elementType": "labels.text",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "poi.business",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "road",
-            "elementType": "labels",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "road",
-            "elementType": "labels.icon",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "road.arterial",
-            "elementType": "labels",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "road.highway",
-            "elementType": "labels",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "road.local",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "transit",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "water",
-            "elementType": "labels.text",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
+            "visibility": "off"
           }
         ]
-      })
-    });
-    console.log("map loaded");
-}
+      },
+      {
+        "featureType": "administrative.neighborhood",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.business",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      }
+    ]
 
+      loader.load().then(async () => {        
+      let new_map = new google.maps.Map(map, {
+        center: { lat: 47.16249, lng: 19.503304},
+        zoom: 7.9,
+        styles: style_sheet
+      })
+      new google.maps.Marker({
+        map: new_map,
+        position: position,
+        title: 'Uluru'
+      }
+      )
+    });
+  }
 }
