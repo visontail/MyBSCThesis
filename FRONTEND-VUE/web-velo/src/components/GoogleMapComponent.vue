777f621c69069f594e07a9502bf95bec47cc6e40
visontail <the051425@gmail.com>
2023-08-30 08:40:38 +0200

    markers work

--- FRONTEND-VUE/web-velo/src/components/GoogleMapComponent.vue
+++ FRONTEND-VUE/web-velo/src/components/GoogleMapComponent.vue
@@ -2,9 +2,9 @@
     <div ref="map" class="map"></div>
 </template>
 
+
 <script>
 
-/* import MarkerComponent from './MarkerComponent.vue'; */
 
 // variables for GoogleMaps API - maps's style sheet, marker's icon
 let style_sheet = [
@@ -103,43 +103,94 @@ let style_sheet = [
       ]
     }
   ]
+  import axios from 'axios'
+const apiUrl = 'http://localhost:8080';
 
+let icon = {
+    path : "M480.059-486.667q30.274 0 51.774-21.559t21.5-51.833q0-30.274-21.559-51.774t-51.833-21.5q-30.274 0-51.774 21.559t-21.5 51.833q0 30.274 21.559 51.774t51.833 21.5ZM480-80Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z",
+    fillColor: "#E1EF79",
+    fillOpacity: 1,
+    strokeWeight: 1,
+    strokeColor: "#000000",
+    scale: 0.03,
+}
 
 
 /* global google */ // <- solving google is not defined error
 export default {
-  name: 'GoogleMap',
-  data() {
-    return {
-      map: null,
-      marker: null,
-      apiKey: 'AIzaSyCrqeOVzVOTRdPZh_VoEN1epBl04KoxJlc'
-    };
-    
-  },
-  // this runs when the web app is loaded
-  mounted() {
-    this.initMap();
-  },
-  methods: {
-    // Function for initializing Google Map
-    initMap() {
-      // load Google Maps API via a script element
-      const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}`;
-      // using create map function to pass map's and marker's variables
-      script.onload = this.createMap;
-      document.head.appendChild(script);
+    name: 'GoogleMap',
+    data() {
+        return {
+            map: null,
+            marker: null,
+            apiKey: 'AIzaSyCrqeOVzVOTRdPZh_VoEN1epBl04KoxJlc'
+        };
     },
-    createMap() {
-      // Create a map centered at a specific location (Hungary)
-      this.map = new google.maps.Map(this.$refs.map, {
-        center: { lat: 47.16249, lng: 19.503304},
-        zoom: 7,
-        styles: style_sheet
-      });
+    // this runs when the web app is loaded
+    mounted() {
+        this.initMap();
+        this.getPos().then(() => {
+          this.fetchPosData();
+        });
     },
-  }
+    methods: {
+        // Function for initializing Google Map
+        initMap() {
+            // load Google Maps API via a script element
+            const script = document.createElement('script');
+            script.src = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}`;
+            // using create map function to pass map's and marker's variables
+            script.onload = this.createMap;
+            document.head.appendChild(script);
+        },
+        createMap() {
+            // Create a map centered at a specific location (Hungary)
+            this.map = new google.maps.Map(this.$refs.map, {
+                center: { lat: 47.16249, lng: 19.503304 },
+                zoom: 7,
+                styles: style_sheet
+            });
+        },
+        // Fumction for getting Stations' positions (lat, lng), ID, and name
+        async getPos() {
+          try {
+              const response = await axios.get(`${apiUrl}/pos`);
+              this.marker = response.data;
+          } catch (error) {
+              console.error('Error fetching data:', error);
+              throw error;
+          }
+        },
+        async  fetchPosData() {
+            try {
+                const positions = this.marker;
+                for (const pos of positions) {
+                    const id = pos.StationID;
+                    const name = pos.StationName;
+                    const lat = parseFloat(pos.posLatitude);
+                    const lng = parseFloat(pos.posLongitude);
+
+                    const marker = new google.maps.Marker({
+                        id: id,
+                        position: { lat, lng },
+                        map: this.map,
+                        icon: icon,
+                        title: name,
+                    });
+                    this.clickMarker(marker)
+                    };
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        },
+        clickMarker(marker) {
+            // Attach click event listener to the marker
+            marker.addListener('click', () => {
+                this.map.setZoom(15);
+                this.map.setCenter(marker.getPosition());
+            });
+        }
+    }
 };
 </script>
 
--- FRONTEND-VUE/web-velo/src/components/MarkerComponent.vue
+++ FRONTEND-VUE/web-velo/src/components/MarkerComponent.vue
@@ -1,67 +1,10 @@
 
+
 <script>
 
-import axios from 'axios'
-
-/* global google */
-
-let icon = {
-    path : "M480.059-486.667q30.274 0 51.774-21.559t21.5-51.833q0-30.274-21.559-51.774t-51.833-21.5q-30.274 0-51.774 21.559t-21.5 51.833q0 30.274 21.559 51.774t51.833 21.5ZM480-80Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z",
-    fillColor: "#E1EF79",
-    fillOpacity: 1,
-    strokeWeight: 1,
-    strokeColor: "#000000",
-    scale: 0.03,
-}
-
-// API URL
-const apiUrl = 'http://localhost:8080';
 
 export default {
-    data() {
-        return {
-            markers: [], 
-        };
-    },
-    // ROLITÓL MEGKÉRDEZNI
-    created() {
-        this.fetchPosData();
-    },
     methods: {
-        // Fumction for getting Stations' positions (lat, lng), ID, and name
-        getPos() {
-        return axios.get(`${apiUrl}/pos`)
-            .then(response => response.data)
-            .catch(error => {
-            console.error('Error fetching data:', error);
-            throw error;
-            });
-        },
-        async fetchPosData() {
-            try {
-                const positions = this.getPos().data;
-
-                positions.forEach(pos => {
-                    const id = pos.StationID;
-                    const name = pos.StationName;
-                    const lat = parseFloat(pos.posLatitude);
-                    const lng = parseFloat(pos.posLongitude);
-
-                    const marker = new google.maps.Marker({
-                        id: id,
-                        position: { lat, lng },
-                        map: this.$refs.map,
-                        icon: icon,
-                        title: name,
-                    });
-                    this.clickMarker(marker);
-                    this.markers.push(marker);
-                    });
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-            },
-
         clickMarker(marker) {
             // Attach click event listener to the marker
             marker.addListener('click', () => {
