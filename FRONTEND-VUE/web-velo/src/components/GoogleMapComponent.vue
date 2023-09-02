cf5d4158bf8a0b46699a565091b8380a19d60c16
visontail <the051425@gmail.com>
2023-09-02 20:25:26 +0200

    new map load method implemented

--- FRONTEND-VUE/web-velo/src/components/GoogleMapComponent.vue
+++ FRONTEND-VUE/web-velo/src/components/GoogleMapComponent.vue
@@ -1,28 +1,55 @@
 <template>
-  <GoogleMap api-key="YOUR_GOOGLE_MAPS_API_KEY" style="width: 100%; height: 500px" :center="center" :zoom="15">
-    <Marker :options="{ position: center }" />
+  <GoogleMap
+    api-key="AIzaSyCrqeOVzVOTRdPZh_VoEN1epBl04KoxJlc"
+    style="width: 100%; height: 500px"
+    :center="center"
+    :zoom="4"
+  >
+    <MarkerCluster>
+      <Marker v-for="(location, i) in locations" :options="{ position: location }" :key="i" />
+    </MarkerCluster>
   </GoogleMap>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import { GoogleMap, Marker } from "vue3-google-map";
+import { defineComponent } from 'vue'
+import { GoogleMap, Marker, MarkerCluster } from 'vue3-google-map'
 
 export default defineComponent({
-  components: { GoogleMap, Marker },
+  // eslint-disable-next-line vue/no-reserved-component-names
+  components: { GoogleMap, Marker, MarkerCluster },
   setup() {
-    const center = { lat: 40.689247, lng: -74.044502 };
+    const center = { lat: 47.16249, lng: 19.503304 }
+    const locations = [
+      {
+        lat: 47.2385,
+        lng: 18.6349
+      },
+      {
+        lat: 47.2392,
+        lng: 16.9739
+      },
+      {
+        lat: 46.1113,
+        lng: 18.1848
+      },
+      { lat: 45.9522, lng: 18.6671 },
+    ]
 
-    return { center };
-  },
-});
+    return { center, locations }
+  }
+})
 </script>
-
 
 <!-- 
 
-import { ref, onMounted } from 'vue';
-import { Loader } from '@googlemaps/js-api-loader';
+<template>
+    <div ref="map" class="map"></div>
+</template>
+
+
+<script>
+
 
 // variables for GoogleMaps API - maps's style sheet, marker's icon
 let style_sheet = [
@@ -121,12 +148,10 @@ let style_sheet = [
       ]
     }
   ]
-
-import axios from 'axios'
+  import axios from 'axios'
 const apiUrl = 'http://localhost:8080';
-const apiKey = 'AIzaSyCrqeOVzVOTRdPZh_VoEN1epBl04KoxJlc'
 
-/* let icon = {
+let icon = {
     path : "M480.059-486.667q30.274 0 51.774-21.559t21.5-51.833q0-30.274-21.559-51.774t-51.833-21.5q-30.274 0-51.774 21.559t-21.5 51.833q0 30.274 21.559 51.774t51.833 21.5ZM480-80Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z",
     fillColor: "#E1EF79",
     fillOpacity: 1,
@@ -135,42 +160,43 @@ const apiKey = 'AIzaSyCrqeOVzVOTRdPZh_VoEN1epBl04KoxJlc'
     scale: 0.03,
 }
 
+
+/* global google */ // <- solving google is not defined error
 export default {
     name: 'GoogleMap',
-    setup() {
-      const map = ref(null);
-
-      onMounted(async () => {
-        const loader = new Loader({
-          apiKey: `${apiKey}`,
-          version: 'weekly',
-        })
-        const google = await loader.load();
-        const mapOptions = {
-            center: { lat: 47.16249, lng: 19.503304 },
-            zoom: 7,
-            styles: style_sheet
+    data() {
+        return {
+            map: null,
+            marker: null,
+            apiKey: 'AIzaSyCrqeOVzVOTRdPZh_VoEN1epBl04KoxJlc'
         };
-        map.value = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-        const marker = new google.maps.Marker({
-          position:  { lat: 47.16249, lng: 19.503304 },
-          map: map.value,
-          icon: icon,
-          title: 'Marker'
-        });
-      });
-      return { map };
     },
-
     // this runs when the web app is loaded
     mounted() {
+        this.initMap();
         this.getPos().then(() => {
           this.fetchPosData();
         });
         
     },
     methods: {
+        // Function for initializing Google Map
+        initMap() {
+            // load Google Maps API via a script element
+            const script = document.createElement('script');
+            script.src = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}`;
+            // using create map function to pass map's and marker's variables
+            script.onload = this.createMap;
+            document.head.appendChild(script);
+        },
+        createMap() {
+            // Create a map centered at a specific location (Hungary)
+            this.map = new google.maps.Map(this.$refs.map, {
+                center: { lat: 47.16249, lng: 19.503304 },
+                zoom: 7,
+                styles: style_sheet
+            });
+        },
         // Fumction for getting Stations' positions (lat, lng), ID, and name
         async getPos() {
           try {
@@ -181,6 +207,8 @@ export default {
               throw error;
           }
         },
+
+        
         async  fetchPosData() {
             try {
                 const positions = this.marker;
@@ -189,6 +217,32 @@ export default {
                     const name = pos.StationName;
                     const lat = parseFloat(pos.posLatitude);
                     const lng = parseFloat(pos.posLongitude);
+                    const contentString =
+
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    "(last visited June 22, 2009).</p>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Uluru",
+  });
                     const marker = new google.maps.Marker({
                         id: id,
                         position: { lat, lng },
@@ -196,24 +250,29 @@ export default {
                         icon: icon,
                         title: name,
                     });
-                    this.clickMarker(marker)
+                    this.clickMarker(marker, infowindow)
                     };
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         },
-        clickMarker(marker) {
+        clickMarker(marker, infowindow) {
             // Attach click event listener to the marker
             marker.addListener('click', () => {
                 this.map.setZoom(15);
                 this.map.setCenter(marker.getPosition());
                 console.log("Teszt");
+                infowindow.open({
+      anchor: marker,
+      map: this.map
+    });
             });
         }
     }
-
 };
+</script>
 
+<style>
 
 .map {
   width: 99vw;
@@ -241,4 +300,6 @@ position: absolute;
   background: red;
   min-height: 100vh;
 }
+
+</style>
  -->
