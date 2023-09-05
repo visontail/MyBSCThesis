d756ed8e356c31882b8ad3093a2b3e85065e347e
visontail <the051425@gmail.com>
2023-09-05 17:27:05 +0200

    changes

--- FRONTEND-VUE/web-velo/src/components/GoogleMapComponent.vue
+++ FRONTEND-VUE/web-velo/src/components/GoogleMapComponent.vue
@@ -192,8 +192,7 @@ export default {
     });
 
     return { 
-      mapDiv,
-      positions
+      mapDiv
     };
   },
 }
--- FRONTEND-VUE/web-velo/src/services/API.js
+++ FRONTEND-VUE/web-velo/src/services/API.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-
-
 export default(url='http://localhost:8080') => {
     return axios.create({
         baseURL: url
