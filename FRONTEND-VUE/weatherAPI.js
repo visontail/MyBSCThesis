afbd4cfed1436d9e6be4e6e5050e8aa51f0298b4
visontail <the051425@gmail.com>
2023-10-10 17:02:46 +0200

    weather api started

---
+++ FRONTEND-VUE/weatherAPI.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const options = {
+  method: 'GET',
+  url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+  params: {
+    q: '46.071301,18.233141',
+    days: '3'
+  },
+  headers: {
+    'X-RapidAPI-Key': '29c96c7a85mshf0de203c63bd9e3p1942f6jsnd6241943c4e4',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  }
+};
+
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
+/* 
+// 3 DAY FORECAST WEATHER
+try {
+  const response = await axios.request(options);
+  const forecastData = response.data.forecast.forecastday;
+
+  // Iterate through forecastday array and print each forecast
+  forecastData.forEach((forecast, index) => {
+    console.log(`Day ${index + 1} Forecast:`);
+    console.log(`Date: ${forecast.date}`);
+    console.log(`Condition: ${forecast.day.condition.text}`);
+    console.log(`Max Temperature: ${forecast.day.maxtemp_c}°C`);
+    console.log(`Min Temperature: ${forecast.day.mintemp_c}°C`);
+    console.log('\n');
+  });
+} catch (error) {
+  console.error(error);
+} 
+ */
