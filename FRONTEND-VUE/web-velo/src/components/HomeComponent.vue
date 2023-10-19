43293023a28b89c380fa953b03963f16c2200afc
visontail <the051425@gmail.com>
2023-10-19 15:33:53 +0200

    did it jwt works

--- FRONTEND-VUE/web-velo/src/components/HomeComponent.vue
+++ FRONTEND-VUE/web-velo/src/components/HomeComponent.vue
@@ -32,14 +32,36 @@
   methods: {
     async login() {
       // Küldj egy POST kérést a szervernek a bejelentkezéshez
-      const response = await fetch('http://localhost:8080/login', {
+      const response = await fetch('http://localhost:4000/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ name: this.username, password: this.password }),
-      });
-      console.log(response.status);
+      }).catch(error => {
+  console.error('Fetch error:', error);
+});
+if (response.status === 200){
+    // If the response status is OK (e.g., 200), it's a successful response.
+    const data = await response.json();
+    console.log(data);
+    // Handle the data, e.g., token handling.
+  } else {
+    // If the response status is not OK, it's an error response.
+    // Check the response's content type.
+    const contentType = response.headers.get('content-type');
+    if (contentType && contentType.includes('application/json')) {
+      // If the error response is in JSON format.
+      const errorData = await response.json();
+      console.error('Error:', errorData);
+      // Handle the error data, e.g., display an error message.
+    } else {
+      // If the error response is plain text.
+      const errorText = await response.text();
+      console.error('Error:', errorText);
+      // Handle the error text, e.g., display an error message.
+    }
+  }
     },
   },
   }
