db37db5e9c35c0f34bb6f5b6bc9c4555a4253fc9
visontail <the051425@gmail.com>
2023-10-25 15:59:57 +0200

    routing fix

--- FRONTEND-VUE/web-velo/src/components/MenubarComponent.vue
+++ FRONTEND-VUE/web-velo/src/components/MenubarComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">WebVelo</a>
+      <a class="navbar-brand" :href="homeRoute">WebVelo</a>
       <button
         class="navbar-toggler"
         type="button"
