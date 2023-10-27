6f3685b208932b1d060272a619d65da5aab4f476
visontail <the051425@gmail.com>
2023-10-27 15:37:52 +0200

    token works

--- FRONTEND-VUE/server-velo/verify.js
+++ FRONTEND-VUE/server-velo/verify.js
@@ -12,3 +12,24 @@ export function verifyToken(req, res, next) {
         res.status(400).send('Invalid token');
     }
 }
+
+
+/* 
+
+// Middleware to verify access tokens
+const verifyAccessToken = (req, res, next) => {
+  const accessToken = req.body.token;
+  if (accessToken == null) return res.sendStatus(401);
+  jwt.verify(accessToken, process.env.ACCESS_TOKEN, (err, user) => {
+    if (err) {
+      // Access token is expired or invalid
+      // You can generate a new access token here and send it in the response
+      const newAccessToken = generateAccessToken({ name: user.name });
+      res.json({ accessToken: newAccessToken });
+    } else {
+      // Access token is valid; continue with the request
+      req.user = user;
+      next();
+    }
+  });
+}; */
