b612d9301939bf0e4d93219f81eb699e06039893
visontail <the051425@gmail.com>
2023-11-21 16:53:58 +0100

    lorem edit

--- FRONTEND-VUE/web-velo/src/components/AboutComponent.vue
+++ FRONTEND-VUE/web-velo/src/components/AboutComponent.vue
@@ -3,7 +3,7 @@
         <h1>This is the about page</h1>
         <div id="content">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac ligula vitae nulla venenatis sollicitudin. Quisque nec placerat dui, eu rhoncus purus. Phasellus tortor mauris, vestibulum non cursus sit amet, dictum eu lorem. Maecenas euismod gravida enim. Aenean egestas ante ut magna fermentum, nec tristique leo egestas. Suspendisse et odio id ligula ultricies ornare. Proin quis ullamcorper ipsum, nec tincidunt massa. Integer vel arcu in nibh placerat venenatis.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac ligula vitae nulla venenatis sollicitudin. Quisque nec placerat dui, eu rhoncus purus. Phasellus tortor mauris, vestibulum non cursus sit amet, dictum eu lorem. Maecenas euismod gravida enim. Aenean egestas ante ut magna fermentum, nec tristique leo egestas. Suspendisse et odio id ligula ultricies ornare. Proin quis ullamcorper ipsum, nec tincidunt massa. Integer vel arcu in nibh placerat venenatis.</p>
+            <p>Nullam fringilla lacinia nibh a porta. Phasellus ut ultrices enim. Ut et velit vulputate, accumsan orci vitae, dapibus quam. Mauris ac dolor mauris. Fusce ac faucibus mauris, ac imperdiet augue. Integer at neque condimentum, pretium metus eu, lacinia dolor. Nam sed dui nec nibh dapibus varius.</p>
         </div>
     </div>
 </template>
